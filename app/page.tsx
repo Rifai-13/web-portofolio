eef92532e67434d7179120f5478bea1bcfadae7d
Rifai-13 <130067475+Rifai-13@users.noreply.github.com>
2025-02-08 22:31:35 +0700

    hosting

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+"use client"; // Pastikan ini ada di atas jika menggunakan Next.js App Router
+
+import { useEffect } from "react";
+import { logEvent } from "firebase/analytics";
+import { analytics } from "@/lib/firebase"; // Import analytics dari konfigurasi Firebase
 import { Navbar } from "@/components/Navbar";
 import Main from "./main/page";
+
 export default function Home() {
+  useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, "page_view", {
+        page_title: "Home",
+        page_path: window.location.pathname,
+      });
+    }
+  }, []);
+
   return (
     <>
-    <Navbar />
-    <Main />
+      <Navbar />
+      <Main />
     </>
   );
 }
